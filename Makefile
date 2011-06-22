bd504423feb63d0d5c0f9779f660bec00c8abfc2
Michał Derkacz <michal@Lnet.pl>
2011-06-22 10:19:49 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+include $(GOROOT)/src/Make.inc
+
+TARG=gobject
+CGOFILES=\
+	gobject.go\
+
+include $(GOROOT)/src/Make.pkg
---
+++ gobject_test.go
@@ -0,0 +1,11 @@
+package gobject
+
+import (
+	"testing"
+	"time"
+)
+
+func TestAll(t *testing.T) {
+	Print()
+	time.Sleep(100e9)
+}
