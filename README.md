283969e6c664aca2e7e6c5d123cfe40ac74b8fa5
Michał Derkacz <michal@Lnet.pl>
2011-07-27 16:26:35 +0200

    Some comments added in glib_test.go

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ system (like GTK, GStreamer, and others).
 See example [GTK](https://github.com/ziutek/gtk) bindings or
 [GStreamer](https://github.com/ziutek/gst) bindings for example code.
 
+See [glib_test.go](https://github.com/ziutek/glib/blob/master/glib_test.go) for
+signal/event handling model.
--- glib_test.go
+++ glib_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 )
 
+// GLib values
 func TestValue(t *testing.T) {
 	v1 := uint64(0xdeadbeaf)
 	a := ValueOf(v1)
@@ -24,6 +25,45 @@ func TestValue(t *testing.T) {
 	}
 }
 
+// Signals by ID
+func TestSignal(t *testing.T) {
+	// Object on which signal will be emitted
+	o := NewObject(TYPE_OBJECT, nil)
+
+	// New signal without return value, which can be emitted on TYPE_OBJECT
+	// and accepts one parameter of type TYPE_GO_INT
+	s := NewSignal("sig1", TYPE_NONE, o.Type(), TYPE_GO_INT)
+	t.Logf("Signal: %s", s)
+
+	// Object that have methods which will be used as signal handlers
+	a := A("test_signal")
+
+	// Connect to a.handler method. o will be passed as first argument.
+	o.ConnectSid(s, (*A).handler, &a)
+	// Connect to a.noi_hr method. o wiln not be passed to method.
+	o.ConnectSidNoi(s, (*A).noi_h, &a)
+	// Connect to fh function. o will be passed as first argument.
+	o.ConnectSid(s, fh, nil)
+
+	// Emit signal with 123 as its TYPE_GO_INT argument
+	o.EmitById(s, 123)
+}
+
+// Like TestSignal but uses signal names
+func TestSignalName(t *testing.T) {
+	o := NewObject(TYPE_OBJECT, nil)
+
+	NewSignal("sig2", TYPE_NONE, TYPE_OBJECT, TYPE_GO_INT)
+
+	a := A("test_signal_name")
+
+	o.Connect("sig2", (*A).handler, &a)
+	o.ConnectNoi("sig2", (*A).noi_h, &a)
+	o.Connect("sig2", fh, nil)
+
+	o.Emit("sig2", 456)
+}
+
 type A string
 
 func (a *A) handler(o *Object, i int) {
@@ -38,32 +78,3 @@ func fh(o *Object, i int) {
 	fmt.Printf("fh: %v, %d\n", o, i)
 }
 
-func TestSignal(t *testing.T) {
-	o := NewObject(TYPE_OBJECT, nil)
-
-	s := NewSignal("sig1", TYPE_NONE, TYPE_OBJECT, TYPE_GO_INT)
-	t.Logf("Signal: %s", s)
-
-	a := A("test_signal")
-
-	o.ConnectSid(s, (*A).handler, &a)
-	o.ConnectSidNoi(s, (*A).noi_h, &a)
-	o.ConnectSid(s, fh, nil)
-
-	o.EmitById(s, 123)
-}
-
-
-func TestSignalName(t *testing.T) {
-	o := NewObject(TYPE_OBJECT, nil)
-
-	NewSignal("sig2", TYPE_NONE, TYPE_OBJECT, TYPE_GO_INT)
-
-	a := A("test_signal_name")
-
-	o.Connect("sig2", (*A).handler, &a)
-	o.ConnectNoi("sig2", (*A).noi_h, &a)
-	o.Connect("sig2", fh, nil)
-
-	o.Emit("sig2", 456)
-}
